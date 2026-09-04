3223a935010cbdfa9130c5c102714315dba11176
francucchiari-code <francucchiari>
2026-09-04 09:42:12 -0300

    Avance de la Issue #1: Pantalla principal JavaFX

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,41 +7,41 @@
  */
 
 plugins {
-    // Apply the org.jetbrains.kotlin.jvm Plugin to add support for Kotlin.
     alias(libs.plugins.kotlin.jvm)
-
-    // Apply the application plugin to add support for building a CLI application in Java.
+    alias(libs.plugins.openjfx)
     application
 }
 
 repositories {
-    // Use Maven Central for resolving dependencies.
     mavenCentral()
 }
 
 dependencies {
-    // This dependency is used by the application.
     implementation(libs.guava)
+}
+
+javafx {
+    version = "21.0.2"
+    modules("javafx.controls", "javafx.fxml")
 }
 
 testing {
     suites {
-        // Configure the built-in test suite
         val test by getting(JvmTestSuite::class) {
-            // Use Kotlin Test test framework
             useKotlinTest("2.3.0")
         }
     }
 }
 
-// Apply a specific Java toolchain to ease working on different environments.
 java {
-    toolchain {
-        languageVersion = JavaLanguageVersion.of(21)
-    }
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
+}
+
+kotlin {
+    jvmToolchain(17)
 }
 
 application {
-    // Define the main class for the application.
-    mainClass = "org.example.AppKt"
+    mainClass.set("org.example.AppKt")
 }
